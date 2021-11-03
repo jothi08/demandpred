8637e1598983432e89c0dc0f1296c887319a5dde
Jothi Durairaj <d.jothi8@gmail.com>
2021-11-03 08:58:00 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+app = Flask(__name__)
+model = pickle.load(open('list_of_all_models.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        WeekNumber = int(request.form['WeekNumber'])
+        SalesDepotID=int(request.form['SalesDepotID'])
+        SalesChannelID=int(request.form['SalesChannelID'])
+        RouteID=int(request.form['RouteID'])
+        ClientID=int(request.form['ClientID'])
+        ProductID=int(request.form['ProductID'])
+        NewClientName=request.form['NewClientName']
+        pieces=int(request.form['pieces'])
+        weight=int(request.form['weight'])
+        brand=request.form['brand']
+        Town=request.form['Town']
+        State=request.form['State']
+        NewClientName = LabelEncoder().fit_transform(NewClientName)
+        NewProductName = LabelEncoder().fit_transform( NewProductName )
+        Town = LabelEncoder().fit_transform( Town )
+        State = LabelEncoder().fit_transform( State)
+        brand = LabelEncoder().fit_transform(brand)
+        prediction=model.predict([[WeekNumber,SalesDepotID,SalesChannelID,RouteID,ClientID,ProductID,NewClientName,pieces,weight,brand,Town,State]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="There is no demand for this particular product")
+        else:
+            return render_template('index.html',prediction_text="The Demand for the product is {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
