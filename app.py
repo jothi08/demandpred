1d0c448ec970f4e38c86de1477af60d14621161a
Jothi Durairaj <d.jothi8@gmail.com>
2021-11-23 09:43:05 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+import pandas as pd
+import joblib
+model = pickle.load(open('final_model.pkl', 'rb'))
+train = pd.read_csv('train.csv')
+import Final as final
+
+app = Flask(__name__)
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        WeekNumber = int(request.form['WeekNumber'])
+        SalesDepotID=int(request.form['SalesDepotID'])
+        SalesChannelID=int(request.form['SalesChannelID'])
+        RouteID=int(request.form['RouteID'])
+        ClientID=int(request.form['ClientID'])
+        ProductID=int(request.form['ProductID'])
+        NewClientName=request.form['NewClientName']
+        NewProductName=request.form['NewProductName']
+        piece=int(request.form['piece'])
+        weight=int(request.form['weight'])
+        brand=request.form['brand']
+        Town=request.form['Town']
+        State=request.form['State']
+        p=np.array([[WeekNumber,SalesDepotID,SalesChannelID,RouteID,ClientID,ProductID,NewClientName,NewProductName,piece,weight,brand,Town,State]])
+        d1=pd.DataFrame(p)
+        prediction = final.final_f1(train,d1)
+        
+        output=int(np.mean(np.array(prediction)))
+        if output<0:
+            return render_template('index.html',prediction_texts="There is no demand for this particular product")
+        else:
+            return render_template('index.html',prediction_text="The Demand for the product is {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
